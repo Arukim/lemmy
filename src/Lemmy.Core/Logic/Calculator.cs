d5e8eb52985feb69c4ce990405bc1accbdf66a88
Nikita Smelov <smelovn@gmail.com>
2018-12-10 19:00:06 +0300

    changed InterceptTime to use Vector3.Dot

--- src/Lemmy.Core/Logic/Calculator.cs
+++ src/Lemmy.Core/Logic/Calculator.cs
@@ -1,5 +1,4 @@
-﻿using Lemmy.Core.Entities;
-using System;
+﻿using System;
 using System.Numerics;
 
 namespace Lemmy.Core.Logic
@@ -15,22 +14,22 @@ namespace Lemmy.Core.Logic
 
         public static double InterceptTime(Vector3 targetPos, Vector3 targetSpeed, Vector3 catcherPos, double catcherSpeed)
         {
-            double dX = targetPos.X - catcherPos.X, dY = targetPos.Y - catcherPos.Y, dZ = targetPos.Z - catcherPos.Z;
+            var dPos = targetPos - catcherPos;
 
-            var a = targetSpeed.X * targetSpeed.X + targetSpeed.Y * targetSpeed.Y + targetSpeed.Z * targetSpeed.Z - catcherSpeed * catcherSpeed;
+            var a = Vector3.Dot(targetSpeed, targetSpeed) - catcherSpeed * catcherSpeed;
 
-            var b = 2 * (dX * targetSpeed.X + dY * targetSpeed.Y + dZ *targetSpeed.Z);
+            var b = 2 * Vector3.Dot(dPos, targetSpeed);
 
-            var c = dX * dX + dY * dY + dZ * dZ;
+            var c = Vector3.Dot(dPos, dPos);
 
-            var (q1,q2) = SolveQuad(a, b, c);
+            var (q1, q2) = SolveQuad(a, b, c);
 
             return Math.Max(q1, q2);
         }
 
         public static double VectorIntersectPlane(Vector3 objPos, Vector3 objSpeed, Plane plane)
         {
-            return -(Vector3.Dot(plane.Normal, objPos) + plane.D) / Vector3.Dot(plane.Normal, objSpeed); 
+            return -(Vector3.Dot(plane.Normal, objPos) + plane.D) / Vector3.Dot(plane.Normal, objSpeed);
         }
     }
 }
