a5f25f6da3df2a4ade444e3a20fc7b99cf8ade39
Nikita Smelov <smelovn@gmail.com>
2018-12-17 23:25:49 +0300

    tool to create zip_archive from sources

---
+++ src/Lemmy.Tools.SolutionComposer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Lemmy.Tools.SolutionComposer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Delete("lemmy.zip");
+            using (var archive = ZipFile.Open("lemmy.zip", ZipArchiveMode.Create))
+            {
+                var files = Directory.GetFiles(args[0], "*.cs", SearchOption.AllDirectories);
+                var counter = 0;
+                foreach (var file in files)
+                {
+                    var targetFileName = $"{counter++}";
+                    if (string.Equals(Path.GetFileNameWithoutExtension(file), "MyStrategy"))
+                    {
+                        targetFileName = "MyStrategy";
+                    }
+                    archive.CreateEntryFromFile(file, $"{targetFileName}.cs");
+                }
+            }
+        }
+    }
+}
